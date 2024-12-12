49b20a931d0b3ab13eee91008357b7aa968f7494
pollog09 <jgnzalez19@gmail.com>
2024-12-12 17:49:34 -0600

    first commit

---
+++ check_gpu.py
@@ -0,0 +1,11 @@
+import torch
+print("Starting up...")
+print("PyTorch version:", torch.__version__)
+print("Checking GPU...")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("CUDA is available. Using GPU.")
+else:
+    device = torch.device("cpu")
+    print("CUDA is not available. Using CPU.")
+
