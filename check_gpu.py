75c47023361b2e75bf39c1550d18adc2bc800c2e
pollog09 <jgnzalez19@gmail.com>
2024-12-12 19:10:03 -0600

    Refactor CovidClassificator notebook to improve structure and add markdown sections for clarity; update check_gpu.py to include a new line for better readability.

--- check_gpu.py
+++ check_gpu.py
@@ -2,6 +2,7 @@ import torch
 print("Starting up...")
 print("PyTorch version:", torch.__version__)
 print("Checking GPU...")
+
 if torch.cuda.is_available():
     device = torch.device("cuda")
     print("CUDA is available. Using GPU.")
